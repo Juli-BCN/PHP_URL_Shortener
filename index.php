97dad8cccb6fbed0c8f4fb29ebbce617a16bed56
Juli-BCN <julibcn@gmail.com>
2019-11-26 23:06:28 +0000

    Update index.php

--- index.php
+++ index.php
@@ -12,7 +12,7 @@
 <body>
 
 <div class="container">
-  <h2>LeasePlan AWS EC2/RDS PHP URL shortener</h2>
+  <h2>LeasePlan AWS Docker PHP URL shortener</h2>
   <img src="lp_logo2.png" alt="LeasePlan Logo" /> <img src="aws_logo2.png" alt="AWS Logo" /></p>
 
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
@@ -59,7 +59,7 @@ text-align:center;
 </div>
 
 <div class="footer">
-<p>&copy; Developed by <a href="https://www.leaseplan.com">LeasePlan</a> @ AWS Immersion Day [Dublin 2017]</p>
+<p>&copy; Developed by <a href="https://www.leaseplan.com">LeasePlan</a> @ AWS Immersion Day [Atlanta 2019]</p>
 </div>
 </body>
 </html>
